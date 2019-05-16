4e311fd2f47bfd3b90764b71a1b34dbd9d7d059c
Farshid Ashtari <farshid.ashtari@student.hu.nl>
2019-05-16 13:03:37 +0200

    removed test reports

--- bin/run-container.sh
+++ bin/run-container.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+
+# // deze file werkt nu niet, fix hem later (met deze hoeft je alleen deze file te runnen en hij pusht je result to git)
 docker run -it --name autorun-test -v $PWD/tests/reports:/opt/robotframework/reports:Z -v $PWD/tests/scenarios:/opt/robotframework/tests:Z -e BROWSER=chrome farshid-build-15:latest
 docker cp autorun-test:/opt/robotframework/reports/selenium-screenshot-1.png ./selenium-screenshot-1.png
 git add /bin/tests/reports
 
 # /selenium-screenshot-1.png
-git commit -m "Test results-2"
+git commit -m "Test results-1"
 git push
-# ddadsf ag
