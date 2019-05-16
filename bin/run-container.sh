e3900903ed8d52b47534070325e1e0578dbaa301
Farshid Ashtari <farshid.ashtari@student.hu.nl>
2019-05-16 11:54:19 +0200

    initial-4

--- bin/run-container.sh
+++ bin/run-container.sh
@@ -2,9 +2,9 @@
 
 docker run -it --name autorun-test -v $PWD/tests/reports:/opt/robotframework/reports:Z -v $PWD/tests/scenarios:/opt/robotframework/tests:Z -e BROWSER=chrome farshid-build-15:latest
 docker cp autorun-test:/opt/robotframework/reports/selenium-screenshot-1.png ./selenium-screenshot-1.png
-git add /bin/test/reports
+git add /bin/tests/reports
 
 # /selenium-screenshot-1.png
 git commit -m "Test results-2"
 git push
-# ddadsf a
+# ddadsf ag
